1725a6510a1c0e087b09f9112653f60fd398c507
Kevin Andres <khernandez@growthaccelerationpartners.com>
2025-03-06 16:36:12 -0600

    Interaction with Web Elements

--- tests/firstTest.spec.ts
+++ tests/firstTest.spec.ts
@@ -1,4 +1,4 @@
-import {test} from '@playwright/test';
+import {test, expect} from '@playwright/test';
 
 test.beforeEach(async ({page}) => {
   await page.goto('http://localhost:4200/');
@@ -6,7 +6,7 @@ test.beforeEach(async ({page}) => {
   await page.getByText('Form Layouts').click();
 });
 
-test('Locator syntax rules', async ({page}) => {
+test.skip('Locator syntax rules', async ({page}) => {
   // By Tag Name
   await page.locator('input').first().click();
   
@@ -35,6 +35,82 @@ test('Locator syntax rules', async ({page}) => {
   page.locator(':text-is("Using the Grid")');
 });
 
-test('User facing locators', async ({page}) => {
+test.skip('User facing locators', async ({page}) => {
+  await page.getByRole('textbox', {name: 'Email'}).first().click();
+  await page.getByRole('button', {name: 'Sign in'}).first().click();
+
+  await page.getByLabel('Email').first().click();
+
+  await page.getByPlaceholder('Jane Doe').click();
+
+  await page.getByText('Using the Grid').click();
+
+  await page.getByTestId('SignIn').click();
+
+  await page.getByTitle('IoT Dashboard').click();
+});
+
+test.skip('Locating child elements', async ({page}) => {
+  await page.locator('nb-card nb-radio :text-is("Option 1")').click();
+  await page.locator('nb-card').locator('nb-radio').locator(':text-is("Option 2")').click();
+
+  await page.locator('nb-card').getByRole('button', {name: 'Sign in'}).first().click();
+
+  await page.locator('nb-card').nth(3).getByRole('button').click(); //select by index [avoid this approach]
+});
+
+test.skip('Locating parent elements', async ({page}) => {
+  // nb-card is the parent element, by adding a second argument to the locator a child element can be located
+  await page.locator('nb-card', {hasText: "Using the Grid"}).getByRole('textbox', {name: 'Email'}).click();
+  await page.locator('nb-card', {has: page.locator('#inputEmail1')}).getByRole('textbox', {name: 'Email'}).click();
+
+  await page.locator('nb-card').filter({hasText: "Basic form"}).getByRole('textbox', {name: "Email"}).click();
+  await page.locator('nb-card').filter({has: page.locator('.status-danger')}).getByRole('textbox', {name: 'Password'}).click();
+
+  // Filter by multiple conditions
+  // Action to Perform Click email textbox inside the HOrizontal form
+  // 1. Find the nb-card element
+  // 2. Filter the nb-card element by the checkbox element (at this point 2 nb-card elements are found)
+  // 3. Filter the nb-card element by the text "Sign in" (at this point 1 nb-card element is found)
+  // 4. Find the email textbox inside the nb-card element
+  await page.locator('nb-card').filter({has: page.locator('nb-checkbox')}).filter({hasText: "Sign in"})
+    .getByRole('textbox', {name: "Email"}).click();
+
   
+  await page.locator(':text-is("Using the Grid")').locator('..').getByRole('textbox', {name: 'Email'}).click();
+});
+
+test.skip('Reusing locators', async ({page}) => {
+  const basicForm = page.locator('nb-card').filter({hasText: "Basic form"});
+  const emailField = basicForm.getByRole('textbox', {name: "Email"});
+  const passwordField = basicForm.getByRole('textbox', {name: "Password"});
+  const signInButton = basicForm.getByRole('button');
+
+  await emailField.fill('test@test.com');
+  await passwordField.fill('Welcome123');
+  await basicForm.locator('nb-checkbox').click();
+  await signInButton.click();
+
+  await expect(emailField).toHaveValue('test@test.com');
+});
+
+test('extracting values', async ({page}) => {
+  //single test value
+  const basicForm = page.locator('nb-card').filter({hasText: "Basic form"});
+  const buttonText = await basicForm.locator('button').textContent();
+  expect(buttonText).toEqual('Submit');
+
+  //all text values
+  const allRadioButtonsLabels = await page.locator('nb-radio').allTextContents();
+  expect(allRadioButtonsLabels).toContain('Option 1');
+
+  //input value
+  const emailField = basicForm.getByRole('textbox', {name: "Email"});
+  await emailField.fill('test@test.com');
+  const emailValue = await emailField.inputValue();
+  expect(emailValue).toEqual('test@test.com');
+
+  // get attribute value
+  const emailPlaceholder = await emailField.getAttribute('placeholder');
+  expect(emailPlaceholder).toEqual('Email');
 });
